f5a1e25c367a32630470150f14ea81c64754817c
MafioP <mario.pereda.08@gmail.com>
2026-02-09 19:22:36 +0100

    update pre and post

--- post-tts.sh
+++ post-tts.sh
@@ -1,19 +1,12 @@
-#!/bin/bash                                                                                                             
-PID=$(pidof spotifyd)
-CONTROL_NAME="rs.spotifyd.instance$PID"
-MPRIS_NAME="org.mpris.MediaPlayer2.spotifyd.instance$PID"
+#!/bin/bash
 
-# Wait for spotifyd to appear on D-Bus
-for i in {1..10}; do
-    dbus-send --print-reply --dest=org.freedesktop.DBus /org/freedesktop/DBus \
-        org.freedesktop.DBus.ListNames | grep -q "$CONTROL_NAME" && break
-    sleep 0.3
-done
+PID=$(pidof spotifyd) || exit 0
+MPRIS="org.mpris.MediaPlayer2.spotifyd.instance$PID"
 
-# Transfer playback back to spotifyd (in case another client took over)
-dbus-send --system --print-reply --dest=$CONTROL_NAME /rs/spotifyd/Controls rs.spotifyd.Controls.TransferPlayback
+dbus-send --system \
+  --dest="$MPRIS" \
+  /org/mpris/MediaPlayer2 \
+  org.mpris.MediaPlayer2.Player.Play \
+  >/dev/null 2>&1 &
 
-# Resume playback
-dbus-send --system --print-reply --dest=$MPRIS_NAME /org/mpris/MediaPlayer2 \
-    org.mpris.MediaPlayer2.Player.Play
-
+exit 0
--- pre-tts.sh
+++ pre-tts.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-PID=$(pidof spotifyd)
-CONTROL_NAME="rs.spotifyd.instance$PID"
-MPRIS_NAME="org.mpris.MediaPlayer2.spotifyd.instance$PID"
+PID=$(pidof spotifyd) || exit 0
+MPRIS="org.mpris.MediaPlayer2.spotifyd.instance$PID"
 
-# Wait for spotifyd to appear on D-Bus
-for i in {1..10}; do
-    dbus-send --print-reply --dest=org.freedesktop.DBus /org/freedesktop/DBus \
-        org.freedesktop.DBus.ListNames | grep -q "$CONTROL_NAME" && break
-    sleep 0.3
-done
+dbus-send --system \
+  --dest="$MPRIS" \
+  /org/mpris/MediaPlayer2 \
+  org.mpris.MediaPlayer2.Player.Pause \
+  >/dev/null 2>&1 &
 
-# Transfer playback
-dbus-send --system --print-reply --dest=$CONTROL_NAME /rs/spotifyd/Controls rs.spotifyd.Controls.TransferPlayback
-
-# Now you can safely call MPRIS methods
-dbus-send --system --print-reply --dest=$MPRIS_NAME /org/mpris/MediaPlayer2 \
-    org.mpris.MediaPlayer2.Player.Pause
+exit 0
